22c2d5cd5f516fe483bc13b0cf829139f4af1e7a
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-04-08 16:59:45 -0700

    Add code for pages

--- PlantCare/PlantCare/PlantNameViewController.swift
+++ PlantCare/PlantCare/PlantNameViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class PlantNameViewController: UIViewController {
-
+    @IBOutlet weak var scientificNameLabel: UITextField!
+    @IBOutlet weak var nicknameLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func onNext(_ sender: Any) {
+        if (scientificNameLabel != nil) {
+            // self.performSegue(withIdentifier: "identifier", sender: nil)
+        } else {
+            print("Please enter scientific name.")
+        }
     }
     
 
